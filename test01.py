9a4d50135938c4c023939a9d808b74459c97744c
Beamayumi <beamayumia.31@gmail.com>
2026-08-17 19:48:26 -0300

    Hogwarts
    Ideia das variáveis, usando o print, input e format.

---
+++ test01.py
@@ -0,0 +1,62 @@
+a = 'Harry discovered that a scar can hold a story much bigger than it seems.'
+b = 'Hogwarts was not just a school, it was the beginning of great adventures.'
+c = 'Hermione proved that knowledge can be just as powerful as magic.'
+d = 'Ron showed that a true friend stays by your side during difficult times.'
+e = 'The invisibility cloak transformed simple corridors into paths full of mystery.'
+f = 'The Hogwarts Express was the first step into a completely different world.'
+g = 'Hagrid showed that frightening creatures can also have a huge heart.'
+h = 'Dumbledore taught that wisdom did not come only from books.'
+i = 'The Forbidden Forest hid dangers that few students would have the courage to face.'
+j = 'The Sorting Hat could reveal a lot about a wizard’s personality.'
+k = 'Gryffindor was known for its courage in the face of the greatest challenges.'
+l = 'Slytherin carried a history marked by ambition, mystery, and rivalries.'
+m = 'Ravenclaw valued intelligence and curiosity above all else.'
+n = 'Hufflepuff showed that loyalty and dedication were also forms of strength.'
+o = 'Quidditch turned the skies of Hogwarts into a true battlefield.'
+p = 'Quidditch matches made Harry forget for a few minutes the problems surrounding him.'
+q = 'Sirius Black showed that some stories from the past can hide great injustices.'
+r = 'Neville started out insecure, but showed that courage can grow over time.'
+s = 'Draco Malfoy represented a rivalry that followed Harry throughout his years at Hogwarts.'
+t = 'Snape was a character surrounded by secrets that were only understood in the end.'
+u = 'The Chamber of Secrets revealed that Hogwarts could hide very ancient dangers.'
+v = 'The Marauder’s Map turned the castle corridors into a great mystery.'
+w = 'The Horcruxes showed how far someone could go in search of power and immortality.'
+x = 'The Triwizard Tournament placed Harry in challenges he never expected to face.'
+y = 'The Goblet of Fire brought competitions, dangers, and unexpected events to Hogwarts.'
+z = 'Dumbledore’s Army showed that students could also unite to face difficult times.'
+aa = 'Bellatrix Lestrange represented one of the most dangerous sides of the followers of darkness.'
+ab = 'Voldemort spent years trying to gain a power that ended up destroying his own humanity.'
+ac = 'The Battle of Hogwarts showed that courage can emerge even when everything seems lost.'
+welcome = 'Welcome to Hogwarts'
+hogwarts = input('Hello, let’s get started! Type Hogwarts and we will see the magic of programming happening! :) :')
+
+print('{}'.format(welcome))
+print('{}'.format(a))
+print('{}'.format(b))
+print('{}'.format(c))
+print('{}'.format(d))
+print('{}'.format(e))
+print('{}'.format(f))
+print('{}'.format(g))
+print('{}'.format(h))
+print('{}'.format(i))
+print('{}'.format(j))
+print('{}'.format(k))
+print('{}'.format(l))
+print('{}'.format(m))
+print('{}'.format(n))
+print('{}'.format(o))
+print('{}'.format(p))
+print('{}'.format(q))
+print('{}'.format(r))
+print('{}'.format(s))
+print('{}'.format(t))
+print('{}'.format(u))
+print('{}'.format(v))
+print('{}'.format(w))
+print('{}'.format(x))
+print('{}'.format(y))
+print('{}'.format(z))
+print('{}'.format(aa))
+print('{}'.format(ab))
+print('{}'.format(ac))
