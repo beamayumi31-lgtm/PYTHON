16ed194b036503d0fad51b30b2b5f834853d5bb4
Beamayumi <beamayumia.31@gmail.com>
2026-08-24 19:58:02 -0300

    Add Python script to solve a math problem

---
+++ 100exercises/02.py
@@ -0,0 +1,13 @@
+print('A diferença entre um número e seu dobro é 20. Qual é esse número?')
+# A diferença entre um número e seu dobro é 20.
+# x - 2x = 20
+# -x = 20
+# x = -20
+
+x = -20
+dobro_de_x = x*2
+resultado = x - dobro_de_x
+
+print(f"O número é {x}.")
+print(f"O dobro é {dobro_de_x}.")
+print(f"A diferença entre {x} e {dobro_de_x} é {resultado}.")
