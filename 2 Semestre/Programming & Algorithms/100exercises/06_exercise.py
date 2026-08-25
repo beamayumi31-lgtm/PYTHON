e96eefab1b1929f84bd986285a3116e7692afe69
Beamayumi <beamayumia.31@gmail.com>
2026-08-25 01:54:55 -0300

    06_exercise.py
    Largura, altura, área e perímetro.

---
+++ 2 Semestre/Programming & Algorithms/100exercises/06_exercise.py
@@ -0,0 +1,6 @@
+largura = int(input('Largura: '))
+altura = int(input('Altura: '))
+área = largura * altura
+perímetro = 2*(largura+altura)
+print('Área: {}'.format(área))
+print('Perímetro: {}'.format(perímetro))
