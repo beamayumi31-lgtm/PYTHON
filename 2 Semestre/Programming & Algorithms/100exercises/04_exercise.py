1ee4d1d7445dda08a4ad79d6f62cd266743568d8
Beamayumi <beamayumia.31@gmail.com>
2026-08-25 01:41:35 -0300

    04_exercise.py
    Dobro, triplo e metade de um número inteiro.

---
+++ 2 Semestre/Programming & Algorithms/100exercises/04_exercise.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número:'))
+dobro = n1 * 2
+triplo = n1 * 3
+metade = n1//2
+print('Dobro: {}'.format(dobro))
+print('Triplo: {}'.format(triplo))
+print('Metade: {}'.format(metade))
