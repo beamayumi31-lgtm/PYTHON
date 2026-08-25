fa17a5c6e92edbe4e96a885f1836caf9db90f891
Beamayumi <beamayumia.31@gmail.com>
2026-08-25 02:01:47 -0300

    Add temperature conversion from Celsius to Fahrenheit
    Adicionar a temperatura em Celsius e converter para Fahrenheit.

---
+++ 2 Semestre/Programming & Algorithms/100exercises/07_exercise.py
@@ -0,0 +1,4 @@
+celsius = int(input('Digite a temperatura em Celsius para converter em Fahrenheit:'))
+fahrenheit = celsius * 9 / 5 + 32
+print('Celsius: {} °C'.format(celsius))
+print('Fahrenheit: {}'.format(fahrenheit))
