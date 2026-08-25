30fffdea3def4d71a81074615555c17b2838ca8b
Beamayumi <beamayumia.31@gmail.com>
2026-08-25 02:04:12 -0300

    Fix Fahrenheit output formatting and conversion type

--- 2 Semestre/Programming & Algorithms/100exercises/07_exercise.py
+++ 2 Semestre/Programming & Algorithms/100exercises/07_exercise.py
@@ -1,4 +1,4 @@
 celsius = int(input('Digite a temperatura em Celsius para converter em Fahrenheit:'))
-fahrenheit = celsius * 9 / 5 + 32
+fahrenheit = int(celsius * 9 / 5 + 32)
 print('Celsius: {} °C'.format(celsius))
-print('Fahrenheit: {}'.format(fahrenheit))
+print('Fahrenheit: {} °F'.format(fahrenheit))
