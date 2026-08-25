b839bb35db95f18aeb373065f70f44a42f12d9de
Beamayumi <beamayumia.31@gmail.com>
2026-08-25 01:50:05 -0300

    05_exercise.py
    Conversão em metros para centímetros e milímetros.

---
+++ 2 Semestre/Programming & Algorithms/100exercises/05_exercise.py
@@ -0,0 +1,6 @@
+metros = int(input('Quantos metros deseja converter? '))
+centímetros = metros*100
+milímetros = metros*1000
+print('Metros: {}'.format(metros))
+print('Centímetros: {}'.format(centímetros))
+print('Milímetros: {}'.format(milímetros))
