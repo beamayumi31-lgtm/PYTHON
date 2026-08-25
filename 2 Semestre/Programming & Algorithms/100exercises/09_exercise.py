7cc48c527ad21a35957a6629fe6505b0b10e7483
Beamayumi <beamayumia.31@gmail.com>
2026-08-25 20:22:40 -0300

    Add salary increase calculation in 09_exercise.py

---
+++ 2 Semestre/Programming & Algorithms/100exercises/09_exercise.py
@@ -0,0 +1,5 @@
+salario_atual = float(input('Salário atual: R$'))
+aumento = salario_atual * 0.15
+novo_salário = salario_atual + aumento
+print('Aumento: R${}'.format(aumento))
+print('Novo salário: R${}'.format(novo_salário))
