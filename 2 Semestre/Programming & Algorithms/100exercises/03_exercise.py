b231a15fe043a01cab3c0f3d27af97344e74c6b3
Beamayumi <beamayumia.31@gmail.com>
2026-08-25 01:34:16 -0300

    Create 03_exercise.py

---
+++ 2 Semestre/Programming & Algorithms/100exercises/03_exercise.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um número:'))
+antecessor = n1-1
+sucessor = n1+1
+print('Antecessor: {}'.format(antecessor))
+print('Número: {}'.format(n1))
+print('Sucessor: {}'.format(sucessor))
